aaa878ba3c8118a07be340caf97d392a2527da95
Michael O'Toole <mikey@homotechsual.dev>
2022-05-04 15:47:15 +0100

    Add new policy related endpoints.

---
+++ Public/New/New-NinjaOnePolicy.ps1
@@ -0,0 +1,51 @@
+
+using namespace System.Management.Automation
+#Requires -Version 7
+function New-NinjaOnePolicy {
+    <#
+        .SYNOPSIS
+            Creates a new policy using the NinjaOne API.
+        .DESCRIPTION
+            Create a new policy using the NinjaOne v2 API.
+        .OUTPUTS
+            A powershell object containing the response.
+    #>
+    [CmdletBinding( SupportsShouldProcess, ConfirmImpact = 'Medium' )]
+    [OutputType([Object])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', '', Justification = 'Uses dynamic parameter parsing.')]
+    Param(
+        # The mode to run in, new, child or copy.
+        [Parameter(Mandatory)]
+        [ValidateSet('NEW', 'CHILD', 'COPY')]
+        [int]$mode,
+        # An object containing the policy to create.
+        [Parameter(Mandatory)]
+        [object]$policy,
+        # The ID of the template policy to copy from.
+        [int]$templatePolicyId,
+        # Show the policy that was created.
+        [switch]$show
+    )
+    try {
+        if ($Mode -eq 'CHILD' -and $null -eq $policy.parentPolicyId) {
+            throw 'The policy must have a parent policy id if using "CHILD" mode.'
+        } elseif ($mode -eq 'COPY' -and $null -eq $templatePolicyId) {
+            throw 'The policy must have a template policy id if using "COPY" mode.'
+        }
+        $Resource = 'v2/policies'
+        $RequestParams = @{
+            Resource = $Resource
+            Body = $location
+        }
+        if ($PSCmdlet.ShouldProcess("Policy '$($policy.name)'", 'Create')) {
+            $PolicyCreate = New-NinjaOnePOSTRequest @RequestParams
+            if ($show) {
+                Return $PolicyCreate
+            } else {
+                Write-Information "Policy '$($PolicyCreate.name)' created."
+            }
+        }
+    } catch {
+        New-NinjaOneError -ErrorRecord $_
+    }
+}
---
+++ Public/Reset/Reset-NinjaOneDevicePolicyOverrides.ps1
@@ -0,0 +1,35 @@
+
+using namespace System.Management.Automation
+#Requires -Version 7
+function Reset-NinjaOneDevicePolicyOverrides {
+    <#
+        .SYNOPSIS
+            Resets (removes) device policy overrides using the NinjaOne API.
+        .DESCRIPTION
+            Resets (removes) all configured device policy overrides using the NinjaOne v2 API.
+        .OUTPUTS
+            A powershell object containing the response.
+    #>
+    [CmdletBinding( SupportsShouldProcess = $true, ConfirmImpact = 'Medium' )]
+    [OutputType([Object])]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', '', Justification = 'Uses dynamic parameter parsing.')]
+    Param(
+        # The device ID to reset policy overrides for.
+        [Parameter(Mandatory = $true)]
+        [string]$deviceId
+    )
+    try {
+        $Resource = "v2/device/$deviceId/policy/overrides"
+        $RequestParams = @{
+            Resource = $Resource
+        }
+        if ($PSCmdlet.ShouldProcess('Device Policy Overrides', 'Reset')) {
+            $Alert = New-NinjaOneDELETERequest @RequestParams
+            if ($Alert -eq 204) {
+                Write-Information 'Device policy overrides reset successfully.'
+            }
+        }
+    } catch {
+        New-NinjaOneError -ErrorRecord $_
+    }
+}
