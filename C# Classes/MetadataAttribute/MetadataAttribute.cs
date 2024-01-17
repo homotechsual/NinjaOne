f5da0bba75cdc5d132631aaba32c8e8ac4ab5653
Mikey O'Toole <mikey@homotechsual.dev>
2024-01-17 16:06:27 +0000

    Update class arrangement.

---
+++ C# Classes/MetadataAttribute/MetadataAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+public class MetadataAttribute : Attribute {
+    private string[] _tags;
+
+    public MetadataAttribute(params string[] tags) {
+        _tags = tags;
+    }
+
+    public IEnumerable<string> GetTags() {
+        foreach (var tag in _tags) yield return tag;
+    }
+}
